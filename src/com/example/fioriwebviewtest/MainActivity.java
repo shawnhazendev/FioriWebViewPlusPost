9e26a01a8307af94fe64a971ad888277cb567907
Shawn Hazen <shawnh@op-retina-8071s-MacBook-Pro.local>
2014-12-26 11:47:45 -0800

    sped up webview, added HttpHelper to see if that would help with ease of calls, removed the actionbar for cleaner look

--- src/com/example/fioriwebviewtest/MainActivity.java
+++ src/com/example/fioriwebviewtest/MainActivity.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.http.Header;
@@ -44,6 +45,7 @@ public class MainActivity extends Activity{
 	HttpResponse response;
 	BufferedReader in;
 	ProgressBar progress;
+	HttpHelper httpHelper;
 	
 	WebView webView;
 	
@@ -52,7 +54,10 @@ public class MainActivity extends Activity{
 		getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		httpHelper = new HttpHelper();
 		webView = (WebView)findViewById(R.id.webView);
+		webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 		progress = (ProgressBar)findViewById(R.id.progress);
 		progress.setVisibility(View.GONE);
 		WebSettings webSettings = webView.getSettings();
@@ -96,7 +101,7 @@ public class MainActivity extends Activity{
 				webView.loadUrl(fioriUrl);
 				try {
 					website = new URI(fioriUrl);
-//					website = new URI("https://fioridemo.att.com/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html?sap-system-login-oninputprocessing=onLogin&sap-urlscheme=&sap-system-login=onLogin&sap-system-login-basic_auth=&sap-client=200&sap-language=EN&sap-accessibility=&sap-login-XSRF=96GMYr1jzvqt-e-AtobLzpeVGDS-SLuR02GsBi7u4ug%3D&sap-system-login-cookie_disabled=true&sap-user=demoemp1&sap-password=initpass1");
+//					website = new URI("https://fioridemo.att.com/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html?sap-system-login-oninputprocessing=onLogin&sap-urlscheme=&sap-system-login=onLogin&sap-system-login-basic_auth=&sap-client=200&sap-language=EN&sap-accessibility=&sap-login-XSRF=96GMYr1jzvqt-e-AtobLzpeVGDS-SLuR02GsBi7u4ug%3D&sap-system-login-cookie_disabled=true&sap-user=fioridev1&sap-password=Test123");
 					
 				} catch (URISyntaxException e) {
 					// TODO Auto-generated catch block
@@ -112,6 +117,8 @@ public class MainActivity extends Activity{
 				progress.setVisibility(View.GONE);
 				Log.d(TAG, "POST EXECUTE");
 				Log.d(TAG, "Login part 3: electric bagaloo"+responseString);
+//				webView.loadData(responseString, "text/html; charset=utf-8", "UTF-8");
+//				webView.loadDataWithBaseURL("", responseString, "text/html; charset=utf-8", "UTF-8", "");
 			}
 
 			@Override
@@ -140,50 +147,70 @@ public class MainActivity extends Activity{
 		            }
 		        }
 		        
-				DefaultHttpClient httpclient2 = new DefaultHttpClient();
-		        HttpPost httpost = new HttpPost(fioriUrl);
-
-		        Log.d(TAG, "Initial sap-system-login-cookie_disabled = "+cookies.get(0).getValue());
-		        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-		        nvps.add(new BasicNameValuePair("sap-login-XSRF", cookies.get(0).getValue()));
-		        nvps.add(new BasicNameValuePair("sap-urlscheme", ""));
-		        nvps.add(new BasicNameValuePair("sap-system-login", "onLogin"));
-		        nvps.add(new BasicNameValuePair("sap-system-login-basic_auth", ""));
-		        nvps.add(new BasicNameValuePair("sap-accessibility", ""));
-		        nvps.add(new BasicNameValuePair("sap-system-login-cookie_disabled", ""));
-		        nvps.add(new BasicNameValuePair("sap-client", "200"));
-		        nvps.add(new BasicNameValuePair("sap-language", "EN"));
-		        nvps.add(new BasicNameValuePair("sap-user", "demoemp1"));
-		        nvps.add(new BasicNameValuePair("sap-password", "initpass1"));
+//				DefaultHttpClient httpclient2 = new DefaultHttpClient();
+//		        HttpPost httpost = new HttpPost(fioriUrl);
+//
+//		        Log.d(TAG, "Initial sap-system-login-cookie_disabled = "+cookies.get(0).getValue());
+//		        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+//		        nvps.add(new BasicNameValuePair("sap-login-XSRF", cookies.get(0).getValue()));
+//		        nvps.add(new BasicNameValuePair("sap-urlscheme", ""));
+//		        nvps.add(new BasicNameValuePair("sap-system-login", "onLogin"));
+//		        nvps.add(new BasicNameValuePair("sap-system-login-basic_auth", ""));
+//		        nvps.add(new BasicNameValuePair("sap-accessibility", ""));
+//		        nvps.add(new BasicNameValuePair("sap-system-login-cookie_disabled", ""));
+//		        nvps.add(new BasicNameValuePair("sap-client", "200"));
+//		        nvps.add(new BasicNameValuePair("sap-language", "EN"));
+//		        nvps.add(new BasicNameValuePair("sap-user", "fioridev1"));
+//		        nvps.add(new BasicNameValuePair("sap-password", "Test123"));
 		        
 //		        [urlRequest2 addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-		        httpost.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		        httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+		        
+		        HashMap<String, String> httpheaders = new HashMap<>();
+		        HashMap<String, String> params = new HashMap<>();
+		        
+		        params.put("sap-login-XSRF", cookies.get(0).getValue());
+		        params.put("sap-urlscheme", "");
+		        params.put("sap-system-login", "onLogin");
+		        params.put("sap-system-login-basic_auth", "");
+		        params.put("sap-accessibility", "");
+		        params.put("sap-system-login-cookie_disabled", "");
+		        params.put("sap-client", "200");
+		        params.put("sap-language", "EN");
+		        params.put("sap-user", "fioridev1");
+		        params.put("sap-password", "Test123");
+		        
+		        httpheaders.put("Content-Type", "application/x-www-form-urlencoded");
+//		        httpost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+//		        httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+		        
+		        String localresponseString = httpHelper.performGet(fioriUrl, "fioridev1", "Test123", httpheaders);
+//				String localresponseString = httpHelper.performPost(fioriUrl, "fioridev1", "Test123", httpheaders, params);
+				Log.d(TAG, "Login response string " + localresponseString);
 
 		        
-		        response = httpclient2.execute(httpost);
-		        entity = response.getEntity();
+//		        response = httpclient2.execute(httpost);
+//		        entity = response.getEntity();
 		        
 
-		        Log.d(TAG, "Login form get: " + response.getStatusLine());
-		        Header[] headers = response.getAllHeaders();
-		        for(int i=0;i<headers.length;i++){
-		        	Log.d(TAG, "Login headers "+ headers[i]);
-		        }
-		        
-		        Log.d(TAG, "Login set of cookies:");
-		        List<Cookie> loginCookies = httpclient2.getCookieStore().getCookies();
-		        if (loginCookies.isEmpty()) {
-		        	Log.d(TAG, "Login None");
-		        } else {
-		            for (int i = 0; i < loginCookies.size(); i++) {
-		            	Log.d(TAG, "Login - " + loginCookies.get(i).toString());
-		            }
-		        }
+//		        Log.d(TAG, "Login form get: " + response.getStatusLine());
+//		        Header[] headers = response.getAllHeaders();
+//		        for(int i=0;i<headers.length;i++){
+//		        	Log.d(TAG, "Login headers "+ headers[i]);
+//		        }
+//		        
+//		        Log.d(TAG, "Login set of cookies:");
+//		        List<Cookie> loginCookies = httpclient2.getCookieStore().getCookies();
+//		        if (loginCookies.isEmpty()) {
+//		        	Log.d(TAG, "Login None");
+//		        } else {
+//		            for (int i = 0; i < loginCookies.size(); i++) {
+//		            	Log.d(TAG, "Login - " + loginCookies.get(i).toString());
+//		            }
+//		        }
 		        
 
-				String line = EntityUtils.toString(entity);
-				responseString = line;
+//				String line = EntityUtils.toString(entity);
+				responseString = localresponseString;
 				
 				//this code snippit is for if something is needed to be saved to a file
 //				FileOutputStream outputStream;
@@ -194,7 +221,7 @@ public class MainActivity extends Activity{
 //				} catch (Exception e) {
 //				  e.printStackTrace();
 //				}
-				Log.d(TAG, "Login entity content: " + line);
+//				Log.d(TAG, "Login entity content: " + line);
 		        
 		        if (entity != null) {
 		            entity.consumeContent();
@@ -204,7 +231,7 @@ public class MainActivity extends Activity{
 		        // shut down the connection manager to ensure
 		        // immediate deallocation of all system resources
 		        httpclient.getConnectionManager().shutdown();    
-		        httpclient2.getConnectionManager().shutdown();    
+//		        httpclient2.getConnectionManager().shutdown();    
 //		    }
 				}catch(Exception e){
 		        	
